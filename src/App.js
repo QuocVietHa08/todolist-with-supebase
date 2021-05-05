9cd87811f77c85a80c18ed7a051340968688bd84
= <56016006+thebush99@users.noreply.github.com>
2021-05-05 18:25:24 +0700

    project file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import useAuthState from './hook/useAuthState';
+import Todos from './pages/Todo';
+import Auth from './pages/Auth';
+
+function App() {
+  const { user } = useAuthState();
+
+  return <div className='App'>{user ? <Todos /> : <Auth />}</div>;
+}
+
+export default App;
